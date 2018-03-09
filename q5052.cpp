470b20368e79d4c14ab9df42cca017e499860a11
JIYOUN <jyun6902@gmail.com>
2018-03-09 18:51:42 +0900

    전화번호 목록
    방법3

--- q5052.cpp
+++ q5052.cpp
@@ -1,100 +1,87 @@
-#include <cstdio>
-#include <cstring>
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cstdio>
 #include <string>
-#define D_NUM 10
+
 using namespace std;
-struct Trie
+#define NUM 10
+
+struct node {
+	bool is_end;
+	int prefix_cnt;
+	struct node* child[NUM];
+}*head;
+
+void init()
 {
-	bool finish;    //끝나는 지점을 표시해줌
-	Trie* next[D_NUM];
-	Trie() : finish(false)
-	{
-		memset(next, 0, sizeof(next));
-	}
-	~Trie()
-	{
-		for (int i = 0; i < D_NUM; i++)
-			if (next[i] != NULL)
-				delete next[i];
-	}
-	void insert(const char* key)
-	{
-		if (*key == 0)
-			finish = true;    //문자열이 끝나는 지점일 경우 표시
-		else
-		{
-			int curr = *key - '0';
-			if (next[curr] == NULL)
-				next[curr] = new Trie();    //탐색이 처음되는 지점일 경우 동적할당
-			next[curr]->insert(key + 1);    //다음 문자 삽입
+	head = new node();
+	head->is_end = 0;
+	head->prefix_cnt = 0;
+}
 
-		}
-	}
+void insert(int buf_size, string buf)
+{
+	node *current = head;
+	current->prefix_cnt++;
 
-	bool find(const char* key)
+	for (int i = 0; i < buf_size; i++)
 	{
-		if (*key == NULL)
-		{
-			/*for (int i = 0; i < 10; i++)//쓰면 시간초과
-			{
-				if (next[i] != NULL)
-					return true;
-			}*/
-			return false;//문자열이 끝나는 위치를
-		}
+		int letter = (int)(buf[i] - '0');
+		if (current->child[letter] == NULL)
+			current->child[letter] = new node();
 
-		if (finish == 1)
-		{
-			return true;
-		}
-		int curr = *key - '0';
-
-		return next[curr]->find(key + 1); //다음 문자를 탐색
+		current->child[letter]->prefix_cnt++;
+		current = current->child[letter];
 	}
-};
+	current->is_end = true;
+}
+
+bool get_prefix(int buf_size, string buf)
+{
+	node* current = head;
+
+	for (int i = 0; i < buf_size; i++)
+	{
+		int letter = (int)(buf[i] - '0');
+		if (current->child[letter] == NULL)
+			return true;//겹치는 것 없음
+
+		if (current->is_end == true)
+			return false;
+		current = current->child[letter];
+	}
+
+	if (current->prefix_cnt >= 2)
+		return false;
+	else
+		return true;
+}
 
 int main()
 {
-	int N = 0, K = 0;
-	scanf("%d", &N);
-	for (int t_c = 0; t_c < N; t_c++)
+	int t = 0;
+	scanf("%d", &t);
+	for (int t_c = 0; t_c < t; t_c++)
 	{
-		scanf("%d", &K);
-		Trie* ptn = new Trie();
-		vector <string> v;
+		init();
+		int n = 0;
+		scanf("%d", &n);
 		int flag = 0;
-
-		for (int i = 0; i < K; i++)
+		for (int i = 0; i < n; i++)
 		{
-			string s="";
+			string s;
 			cin >> s;
-			v.push_back(s);
-		}
-		sort(v.begin(), v.end());
-		
-		for (int i = 0; i < K; i++)//v.size()
-		{
-			char sen[10] = {};
-			memset(sen, 0, sizeof(sen));
-			for (int j = 0; j < v[i].size(); j++)
-			{
-				sen[j] = v[i][j];
-			}
-			ptn->insert(sen);
-			if (ptn->find(sen))
-			{
+
+			insert(s.size(), s);
+			if (get_prefix(s.size(), s) == 0)
 				flag = 1;
-				printf("NO\n");
-				break;
-			}			
+
 		}
 
 		if (flag == 0)
 			printf("YES\n");
-	}//t_c
+		else
+			printf("NO\n");
+	}//for t_c
 
 	return 0;
 }
