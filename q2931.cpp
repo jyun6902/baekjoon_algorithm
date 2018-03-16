1f734d293baccabe7244050efbed5ae4ce9579c9
JIYOUN <jyun6902@gmail.com>
2018-03-17 00:44:31 +0900

    가스관

--- q2931.cpp
+++ q2931.cpp
@@ -1,207 +1,216 @@
 #include <iostream>
-#include <deque>
 #include <cstring>
+#include <cstdio>
+#include <deque>
 using namespace std;
-/*
-'|'(아스키 124), '-', '+',
-'1 :┌', '2 : └', '3 : ┘', '4 : ┐'
-x, y좌표 헷갈리지 않기!! x; 세로 y; 가로로 놓음
-*/
-int R, C;	//R=세로 C=가로 
-char europe[28][28] = { 0 };
-int visit[28][28], connect[28][28];	//connect= 연결되어있는지 여부
-int dx[4] = { 0, 0, -1, 1 };
-int dy[4] = { 1, -1, 0, 0 };
-int empty_x = 0, empty_y = 0;
-char shape[1];
-deque <pair<int, int >> step;
-
-void result_shape(int x, int y)//빈칸 모양 판별
+char maps[27][27];
+int dx[5] = { 0, 0, 0, 1, -1 };
+int dy[5] = { 0, 1, -1, 0 , 0 };
+bool visit[27][27];
+int R, C = 0;
+//Block ; '|', '-','+','1','2','3','4'
+//1:┌ //2: └ //3:┘ //4: ┐
+//visit체크하는게 중요! 재귀터짐(메모리초과)
+deque <pair<int, int>> dq;
+bool dir[5] = {}; // 동, 서, 남, 북
+void shape(int x, int y)
 {
-	if (europe[x][y - 1] == '-' || europe[x][y - 1] == '+' || europe[x][y - 1] == '1' || europe[x][y - 1] == '2')
-		connect[x][y - 1] = 1;	//연결되어있을 경우 (서)
+	if (maps[x][y + 1] == '-' || maps[x][y + 1] == '+' || maps[x][y + 1] == '3' || maps[x][y + 1] == '4')
+		dir[1] = true;
 	else
-		connect[x][y - 1] = 2;
+		dir[1] = false;
 
-	if (europe[x][y + 1] == '-' || europe[x][y + 1] == '+' || europe[x][y + 1] == '3' || europe[x][y + 1] == '4')
-		connect[x][y + 1] = 1;	//연결되어있을 경우 (동)
+	if (maps[x][y - 1] == '-' || maps[x][y - 1] == '+' || maps[x][y - 1] == '1' || maps[x][y - 1] == '2')
+		dir[2] = true;
 	else
-		connect[x][y + 1] = 2;
+		dir[2] = false;
 
-	if (europe[x - 1][y] == '|' || europe[x - 1][y] == '+' || europe[x - 1][y] == '1' || europe[x - 1][y] == '4')
-		connect[x - 1][y] = 1;	//연결되어있을 경우 (북)
+	if (maps[x + 1][y] == '|' || maps[x + 1][y] == '+' || maps[x + 1][y] == '2' || maps[x + 1][y] == '3')
+		dir[3] = true;
 	else
-		connect[x - 1][y] = 2;
+		dir[3] = false;
 
-	if (europe[x + 1][y] == '|' || europe[x + 1][y] == '+' || europe[x + 1][y] == '2' || europe[x + 1][y] == '3')
-		connect[x + 1][y] = 1;	//연결되어있을 경우 (남)
+	if (maps[x - 1][y] == '|' || maps[x - 1][y] == '+' || maps[x - 1][y] == '1' || maps[x - 1][y] == '4')
+		dir[4] = true;
 	else
-		connect[x + 1][y] = 2;
-
-	if (connect[x][y - 1] == 2 && connect[x][y + 1] == 2 && connect[x + 1][y] == 1 && connect[x - 1][y] == 1)
-		shape[0] = '|';
-
-	else if (connect[x][y - 1] == 1 && connect[x][y + 1] == 1 && connect[x + 1][y] == 2 && connect[x - 1][y] == 2)
-		shape[0] = '-';
-
-	else if (connect[x][y - 1] == 1 && connect[x][y + 1] == 1 && connect[x + 1][y] == 1 && connect[x - 1][y] == 1)
-		shape[0] = '+';
-
-	else if (connect[x][y - 1] == 2 && connect[x][y + 1] == 1 && connect[x + 1][y] == 1 && connect[x - 1][y] == 2)
-		shape[0] = '1';	//┌
-
-	else if (connect[x][y - 1] == 2 && connect[x][y + 1] == 1 && connect[x + 1][y] == 2 && connect[x - 1][y] == 1)
-		shape[0] = '2';	//└
-
-	else if (connect[x][y - 1] == 1 && connect[x][y + 1] == 2 && connect[x + 1][y] == 2 && connect[x - 1][y] == 1)
-		shape[0] = '3';	//┘
-
-	else if (connect[x][y - 1] == 1 && connect[x][y + 1] == 2 && connect[x + 1][y] == 1 && connect[x - 1][y] == 2)
-		shape[0] = '4';	//┐
+		dir[4] = false;
 }
 
-int check(int x, int y)
-{
-	if (x < 1 || x > R || y < 1 || y > C)	//x= 세로 y= 가로
-		return 0;
-	else
-		return 1;
-}
-
-void dfs(int x, int y)
+void cal(int x, int y, int dir)
 {
 	visit[x][y] = 1;
-	int nx = x, ny = y;
-	if (europe[x][y] == '|')
-	{
-		nx = x + 1;
-		ny = y;
-		if (check(nx, ny) == 1 && visit[nx][ny] == 0)
-			dfs(nx, ny);
+	if (!dq.empty())
+		return;
 
-		nx = x - 1;
-		ny = y;
-		if (check(nx, ny) == 1 && visit[nx][ny] == 0)
-			dfs(nx, ny);
+	if (maps[x][y] == '|')
+	{
+		if (dir == 3 && visit[x+1][y] == 0)
+			cal(x + 1, y, 3);
+		else if (dir == 4 && visit[x - 1][y] == 0)
+			cal(x - 1, y, 4);
 	}
 
-	else if (europe[x][y] == '-')
+	else if (maps[x][y] == '-')
 	{
-		nx = x;
-		ny = y - 1;
-		if (check(nx, ny) == 1 && visit[nx][ny] == 0)
-			dfs(nx, ny);
-
-		nx = x;
-		ny = y + 1;
-		if (check(nx, ny) == 1 && visit[nx][ny] == 0)
-			dfs(nx, ny);
+		if (dir == 1 && visit[x][y+1] == 0)
+			cal(x, y + 1, 1);
+		else if (dir == 2 && visit[x][y-1] == 0)
+			cal(x, y - 1, 2);
 	}
 
-	else if (europe[x][y] == '+')
+	else if (maps[x][y] == '+')
 	{
-		for (int i = 0; i < 4; i++)
-		{
-			nx = x + dx[i];
-			ny = y + dy[i];
-			if (check(nx, ny) == 1 && visit[nx][ny] == 0)
-				dfs(nx, ny);
-		}
+		if ( visit[x][y+1] == 0)
+		cal(x, y + 1, 1);
+
+		if ( visit[x][y-1] == 0)
+		cal(x, y - 1, 2);
+		if (visit[x+1][y] == 0)
+		cal(x + 1, y, 3);
+		if ( visit[x-1][y] == 0)
+		cal(x - 1, y, 4);
 	}
 
-	else if (europe[x][y] == '1')		//┌
+	else if (maps[x][y] == '1')//┌
 	{
-		nx = x + 1;
-		ny = y;
-		if (check(nx, ny) == 1 && visit[nx][ny] == 0)
-			dfs(nx, ny);
-
-		nx = x;
-		ny = y + 1;
-		if (check(nx, ny) == 1 && visit[nx][ny] == 0)
-			dfs(nx, ny);
+		if (dir == 2 && visit[x + 1][y] == 0)
+			cal(x + 1, y, 3);
+		else if (dir == 4 && visit[x][y + 1] == 0)
+			cal(x, y + 1, 1);
 	}
 
-	else if (europe[x][y] == '2')		//└
+	else if (maps[x][y] == '2')//└
 	{
-		nx = x;
-		ny = y + 1;
-		if (check(nx, ny) == 1 && visit[nx][ny] == 0)
-			dfs(nx, ny);
-
-		nx = x - 1;
-		ny = y;
-		if (check(nx, ny) == 1 && visit[nx][ny] == 0)
-			dfs(nx, ny);
+		if (dir == 2 && visit[x-1][y] == 0)
+			cal(x - 1, y, 4);
+		else if (dir == 3 && visit[x][y + 1] == 0)
+			cal(x, y + 1, 1);
 	}
 
-	else if (europe[x][y] == '3')		//┘
+	else if (maps[x][y] == '3')//┘
 	{
-		nx = x;
-		ny = y - 1;
-		if (check(nx, ny) == 1 && visit[nx][ny] == 0)
-			dfs(nx, ny);
-
-		nx = x - 1;
-		ny = y;
-		if (check(nx, ny) == 1 && visit[nx][ny] == 0)
-			dfs(nx, ny);
+		if (dir == 1 && visit[x-1][y] == 0)
+			cal(x - 1, y, 4);
+		else if (dir == 3 && visit[x][y - 1] == 0)
+			cal(x, y - 1, 2);
 	}
 
-	else if (europe[x][y] == '4')		//┐
+	else if (maps[x][y] == '4')// ┐
 	{
-		nx = x;
-		ny = y - 1;
-		if (check(nx, ny) == 1 && visit[nx][ny] == 0)
-			dfs(nx, ny);
-
-		nx = x + 1;
-		ny = y;
-		if (check(nx, ny) == 1 && visit[nx][ny] == 0)
-			dfs(nx, ny);
+		if (dir == 1 && visit[x+1][y] == 0)
+			cal(x + 1, y, 3);
+		else if (dir == 4 && visit[x][y - 1] == 0)
+			cal(x, y - 1, 2);
 	}
 
-	else if (europe[x][y] == '.')		//빈공간
+	else if (maps[x][y] == '.')
 	{
-		empty_x = x;
-		empty_y = y;
-		result_shape(empty_x, empty_y);
+		if (x <= R && x >= 1 && y <= C && y >= 1)
+			dq.push_back({ x,y });
+		
+			return;
 	}
+
 }
-
-
 
 int main()
 {
+	int mx, my, zx, zy = 0;
+	memset(maps, 0, sizeof(maps));
 	cin >> R >> C;
-
 	for (int i = 1; i <= R; i++)
 	{
 		for (int j = 1; j <= C; j++)
 		{
-			cin >> europe[i][j];
-			if (europe[i][j] == 'M')	//출발점
-				step.push_back({ i,j });
+			scanf("%1s", &maps[i][j]);
+			if (maps[i][j] == 'M')
+			{
+				mx = i;
+				my = j;			
+			}
+
+			else if (maps[i][j] == 'Z')
+			{
+				zx = i;
+				zy = j;
+			}
 		}
 	}
 
-	int x = step.front().first;
-	int y = step.front().second;
-	int cnt = 0;
-	step.pop_front();
-
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i <= R + 1; i++)
 	{
-		int n_x = x + dx[i];
-		int n_y = y + dy[i];
-
-		if (n_x < 1 || n_x > R || n_y < 1 || n_y > C)
-			continue;
-
-		if (europe[n_x][n_y] != '.')
-			dfs(n_x, n_y);
+		maps[i][0] = '.';
+		maps[i][C + 1] = '.';
 	}
 
-	cout << empty_x << " " << empty_y << " " << shape[0] << endl;
+	for (int j = 0; j <= C + 1; j++)
+	{
+		maps[0][j] = '.';
+		maps[R + 1][j] = '.';
+	}
+	int flag = 0;
+
+	if (maps[zx][zy-1] == '.' && maps[zx][zy + 1] == '.'&& maps[zx+1][zy] == '.' && maps[zx-1][zy] == '.')
+	maps[zx][zy] = '+';
+
+
+	if (maps[mx][my - 1] == '.' && maps[mx][my + 1] == '.'&& maps[mx + 1][my] == '.' && maps[mx - 1][my] == '.')
+	{
+		maps[mx][my] = '+';
+		flag = 1;
+	}
+
+	if (flag == 0)
+	{
+		for (int i = 1; i <= 4; i++)
+		{
+			if (maps[mx + dx[i]][my + dy[i]] != '.')
+			{
+				cal(mx + dx[i], my + dy[i], i);
+				break;
+			}
+		}
+	}
+
+	else //if (flag == 0)//M =근처에 아무것도 없음
+	{
+		for (int i = 1; i <= 4; i++)
+		{
+			if (maps[zx + dx[i]][zy + dy[i]] != '.')
+			{
+				cal(zx + dx[i], zy + dy[i], i);
+				break;
+			}
+		}
+	}
+
+	int x = dq.front().first;
+	int y = dq.front().second;
+	dq.pop_front();
+
+	cout << x << " " << y << " ";
+	shape(x, y);
+
+	if (dir[1] == 1 && dir[2] == 1 && dir[3] == 1 && dir[4] == 1)
+		cout << "+" << endl;
+
+	else if (dir[1] == 0 && dir[2] == 0 && dir[3] == 1 && dir[4] == 1)
+		cout << "|" << endl;
+
+	else if (dir[1] == 1 && dir[2] == 1 && dir[3] == 0 && dir[4] == 0)
+		cout << "-" << endl;
+
+	else if (dir[1] == 1 && dir[2] == 0 && dir[3] == 1 && dir[4] == 0)
+		cout << "1" << endl;
+
+	else if (dir[1] == 1 && dir[2] == 0 && dir[3] == 0 && dir[4] == 1)
+		cout << "2" << endl;
+
+	else if (dir[1] == 0 && dir[2] == 1 && dir[3] == 0 && dir[4] == 1)
+		cout << "3" << endl;
+
+	else if(dir[1] == 0 && dir[2] == 1 && dir[3] == 1 && dir[4] == 0)
+		cout << "4" << endl;
+
 	return 0;
 }
